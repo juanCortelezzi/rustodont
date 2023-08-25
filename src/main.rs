9351818a7e34d515a694a774baabad9c5c2c2c26
J.B.C <juanbicortelezzi@gmail.com>
2023-08-25 21:53:55 +1200

    logs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 mod todonts;
 
+use anyhow::Result;
 use axum::{
     async_trait,
     extract::{FromRef, FromRequestParts, Path},
@@ -12,6 +13,8 @@ use axum::{
 use sqlx::sqlite::{SqlitePool, SqlitePoolOptions};
 use std::net::SocketAddr;
 use todonts::{InputTodont, Todont};
+use tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 struct DBConn(sqlx::pool::PoolConnection<sqlx::Sqlite>);
 
@@ -35,6 +38,7 @@ where
     }
 }
 
+#[tracing::instrument]
 async fn get_all(DBConn(mut conn): DBConn) -> (StatusCode, Json<Vec<Todont>>) {
     let todonts = sqlx::query_as!(Todont, "SELECT * FROM todonts")
         .fetch_all(&mut *conn)
@@ -44,6 +48,7 @@ async fn get_all(DBConn(mut conn): DBConn) -> (StatusCode, Json<Vec<Todont>>) {
     (StatusCode::OK, Json(todonts))
 }
 
+#[tracing::instrument]
 async fn get_one(
     DBConn(mut conn): DBConn,
     Path(id): Path<i64>,
@@ -56,16 +61,19 @@ async fn get_one(
     (StatusCode::OK, Json(todont))
 }
 
+#[tracing::instrument]
 async fn create(DBConn(mut conn): DBConn, Json(payload): Json<InputTodont>) -> StatusCode {
     todonts::create_todont(&mut conn, payload).await.unwrap();
     StatusCode::OK
 }
 
+#[tracing::instrument]
 async fn delete(DBConn(mut conn): DBConn, Path(id): Path<i64>) -> StatusCode {
     todonts::delete_todont(&mut conn, id).await.unwrap();
     StatusCode::OK
 }
 
+#[tracing::instrument]
 async fn update(
     DBConn(mut conn): DBConn,
     Path(id): Path<i64>,
@@ -85,23 +93,87 @@ async fn update(
 }
 
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
+async fn main() -> Result<()> {
+    dotenvy::dotenv()?;
+
+    let database_url = std::env::var("DATABASE_URL").unwrap_or(String::from("sqlite://default.db"));
+    let environment_mode = std::env::var("ENV").unwrap_or(String::from("DEV"));
+    let addr = SocketAddr::from((
+        [0, 0, 0, 0],
+        std::env::var("PORT")
+            .unwrap_or(String::from("3000"))
+            .parse::<u16>()?,
+    ));
+
+    println!(
+        "hello there running the server: {}, {}",
+        database_url, environment_mode,
+    );
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                "rustodont=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     // let mut connection = SqliteConnection::connect("sqlite://sqeel.db").await?;
     let pool = SqlitePoolOptions::new()
         .max_connections(5)
         // .connect("sqlite://sqeel.db")
-        .connect("file::memory:")
+        .connect(&database_url)
         .await?;
 
-    tracing_subscriber::fmt::init();
+    sqlx::migrate!("./migrations").run(&pool).await?;
 
     let app = Router::new()
         .route("/", get(get_all).post(create))
         .route("/:id", get(get_one).put(update).delete(delete))
-        .with_state(pool);
+        .with_state(pool)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &axum::http::Request<_>| {
+                    // Log the matched route's path (with placeholders not filled in).
+                    // Use request.uri() or OriginalUri if you want the real path.
+                    let matched_path = request
+                        .extensions()
+                        .get::<axum::extract::MatchedPath>()
+                        .map(axum::extract::MatchedPath::as_str);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+                    tracing::info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        matched_path,
+                        some_other_field = tracing::field::Empty,
+                    )
+                })
+                .on_request(|_request: &axum::http::Request<_>, _span: &tracing::Span| {
+                    // You can use `_span.record("some_other_field", value)` in one of these
+                    // closures to attach a value to the initially empty field in the info_span
+                    // created above.
+                })
+                .on_response(
+                    |_response: &axum::response::Response,
+                     _latency: std::time::Duration,
+                     _span: &tracing::Span| {
+                        // ...
+                    },
+                )
+                .on_failure(
+                    |_error: ServerErrorsFailureClass,
+                     _latency: std::time::Duration,
+                     _span: &tracing::Span| {
+                        // ...
+                    },
+                ),
+        );
+
     tracing::debug!("listening on {}", addr);
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
